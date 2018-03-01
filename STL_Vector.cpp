a4d2802504e54562627b2658ad1c64c3789741e9
Adesh Agarwal <adeshagarwal02@gmail.com>
2018-03-01 23:22:58 +0530

    c++

--- STL_Vector.cpp
+++
@@ -1,4 +0,0 @@
-/* The most general purpose container is the Vector 
- * it supports a Dynamic Array*/
-#include<iostream>
-using namespace std;
--- list.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<list>
-using namespace std;
-int main()
-{
-	list <int> mylist;
-	return 0;
-}
