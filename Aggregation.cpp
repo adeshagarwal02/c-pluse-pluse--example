937d0b40241af4636219853d89cb25a16fe8d006
Adesh Agarwal <adeshagarwal02@gmail.com>
2020-07-19 18:58:12 +0530

    Inheritance

---
+++ Aggregation.cpp
@@ -0,0 +1,24 @@
+/*******************************************************************************************************************************************************************
+ *			Aggregation
+ *
+ *It is a specialized form of Association where all objects have their own life cycle but there is ownwership & a child object cannot belong to another parent object  
+ *Aggregation is weaker relationship.
+ *Aggregation object is not a part of the container & it can exit independently.
+ *Has are relationship has ownership but each object can servive independently.
+ *                                 
+ *
+ *                                       Bed
+ *					  |	
+ *					  |	
+ *                           chair -->  Room  <-- table
+ *					  |					
+ *                                        |
+ *					Cupboard
+ *
+ *
+ *In c++ aggregation is a procces in which one class defines another class as any entity  reference. It is another way to resuse the calss. It is a form of association 
+ * that represents Has a relationship.
+ * 
+ *
+ *
+ * *****************************************************************************************************************************************************************/
---
+++ Association.cpp
@@ -0,0 +1,11 @@
+/********************************************************************************************************************************
+ *				Association
+ *It is relationship between two seprate classes which establish through their objects. 
+ *Association can be one-to-one, one-to-many, many-to-one and many-to-many.
+ *In object orented programing, an object communicates to object to use functionality and serviceprovided by that object.
+ *Composition & Aggregationare two forms of association.
+ *Has a relastionship 
+ *
+ *
+ *
+ * *****************************************************************************************************************************/
